8471335e06664befeb6f861f1b4d13595cb8f32a
SeyitanShalom <125393865+SeyitanShalom@users.noreply.github.com>
2023-06-12 12:59:07 +0000

    Add files via upload

--- Portfolio.js
+++ Portfolio.js
@@ -1,8 +1,20 @@
 "use strict";
 
-const header = document.querySelector("header");
-const sticky = document.querySelector(".sticky");
+// const header = document.querySelector("header");
+// const sticky = document.querySelector(".sticky");
 
-window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 100);
-});
+// window.addEventListener("scroll", function () {
+//   header.classList.toggle("sticky", window.scrollY > 100);
+// });
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "seyitanoluwapelumi@gmail.com",
+    Password: "shalom03@",
+    To: "seyitanoluwapelumi@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "New Contact Form",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
